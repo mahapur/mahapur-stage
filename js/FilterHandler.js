7b8386d01879584d07d26e8a41c4628328944089
Shriyog Ingale <shriyog@hotstar.com>
2019-08-12 04:01:40 +0530

    Add filter UI elements

---
+++ js/FilterHandler.js
@@ -0,0 +1,8 @@
+function filter() {
+    const city = document.getElementById("city");
+    const category = document.getElementById("category");
+
+    console.log(city.value)
+    console.log(category.value)
+    return false;
+}
